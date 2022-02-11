6832d9cc7ac57f221275ce735fe499541fb52b61
danaalasfour <47094173+danaalasfour@users.noreply.github.com>
2022-02-10 19:27:47 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <vector>
 
+
+/*
+were just creating a board 
+no param
+return vector and it will look like this -8-8-8
+                                         -8-8-8
+                                         -8-8-8
+*/
 std::vector<std::vector<int>> CreateBoard(){
     std::vector<std::vector<int>> vector1;
     for(int i=0;i<3;i++){
@@ -13,6 +21,11 @@ std::vector<std::vector<int>> CreateBoard(){
     }
     return vector1;
 }
+/*
+this displays the board 
+param is vector 1
+prints out the board 
+*/
 
 void DisplayBoard(std::vector<std::vector<int>> vector1){
     std::cout <<"board: "<<std::endl;
@@ -24,12 +37,23 @@ void DisplayBoard(std::vector<std::vector<int>> vector1){
     }
 }
 
+/*
+updates the marker location at the bard
+param is board,loc(location, and player 1
+the marker is either 1 or 0. 
+*/
+
 
 void PlaceMarker(std::vector<std::vector<int>> *board,std::vector<int> loc,int player1){
     int row=loc[0];
     int col=loc[1];
     board[0][row][col] =player1;
 }
+/*
+this gives the player the choice for row and column options
+param is none
+returns the players options
+*/
 
 std::vector<int> GetPlayerChoice(){
     std::vector<int> Options;
@@ -48,19 +72,19 @@ std::vector<int> GetPlayerChoice(){
 
 int main(){
     std::vector<std::vector<int>> board=CreateBoard();
-    int player1=0;
-    for(int i=0;i<12;i++){
+    int player1=0; //set the player 1 marker to 0
+    for(int i=0;i<12;i++){ //iterates through the board about 12 movement if possible
         DisplayBoard(board);
         std::vector<int> Options = GetPlayerChoice();
         PlaceMarker(&board,Options,player1);
         if(player1==0){
-            player1=1;
+            player1=1;//set it to 1
         }
         else{
-            player1 =0;
+            player1 =0; //set it to 0
         }
        
     }
-    DisplayBoard(board);
+    DisplayBoard(board); //print the board
     return 0;
 }
