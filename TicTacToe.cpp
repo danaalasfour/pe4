7c6f3e5ddb23f48c1dffe19966ca68872f92950c
danaalasfour <47094173+danaalasfour@users.noreply.github.com>
2022-02-10 19:58:51 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,6 +1,15 @@
+
 #include <iostream>
 #include <vector>
 
+
+/*
+were just creating a board 
+no param
+return vector and it will look like this 8 8 8
+                                         8 8 8
+                                         8 8 8
+*/
 std::vector<std::vector<int>> CreateBoard(){
     std::vector<std::vector<int>> vector1;
     for(int i=0;i<3;i++){
@@ -12,6 +21,12 @@ std::vector<std::vector<int>> CreateBoard(){
     }
     return vector1;
 }
+/*
+this displays the board 
+param is vector 1
+prints out the board 
+*/
+
 void DisplayBoard(std::vector<std::vector<int>> vector1){
     std::cout <<"board: "<<std::endl;
     for(int i=0;i<vector1.size();i++){
@@ -22,7 +37,54 @@ void DisplayBoard(std::vector<std::vector<int>> vector1){
     }
 }
 
+/*
+updates the marker location at the bard
+param is board,loc(location, and player 1
+the marker is either 1 or 0. 
+*/
+
+
+void PlaceMarker(std::vector<std::vector<int>> *board,std::vector<int> loc,int player1){
+    int row=loc[0];
+    int col=loc[1];
+    board[0][row][col] =player1;
+}
+/*
+this gives the player the choice for row and column options
+param is none
+returns the players options
+*/
+
+std::vector<int> GetPlayerChoice(){
+    std::vector<int> Options;
+    int rowOption = 1;
+    int colOption = 0;
+    std::cout<< "Enter option for row: ";
+    std::cin >>rowOption;
+    std::cout <<std::endl;
+    std::cout << "Please enter columns options: ";
+    std::cin>>colOption;
+    std::cout << std::endl;
+    Options.push_back(rowOption);
+    Options.push_back(colOption);
+    return Options;
+}
 
 int main(){
-  CreateBoard();
+    std::vector<std::vector<int>> board=CreateBoard();
+    int player1=0; //set the player 1 marker to 0
+    for(int i=0;i<9;i++){ //iterate 9 times because that the number of movement when the board fills up completley
+        DisplayBoard(board);
+        std::vector<int> Options = GetPlayerChoice();
+        PlaceMarker(&board,Options,player1);
+        if(player1==0){
+            player1=1;//set it to 1
+        }
+        else{
+            player1 =0; //set it to 0
+        }
+       
+    }
+    DisplayBoard(board); //print the board
+    return 0;
 }
